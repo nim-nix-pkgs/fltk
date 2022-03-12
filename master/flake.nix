3e0f8547a653ff163a1386643e71142d8917f68e
hugosenari <hugosenari@gmail.com>
2022-03-12 02:24:24 -0300

    chore: re index fltk

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''The Fast-Light Tool Kit'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-fltk-master.flake = false;
+  inputs.src-fltk-master.owner = "Skrylar";
+  inputs.src-fltk-master.ref   = "refs/heads/master";
+  inputs.src-fltk-master.repo  = "nfltk";
+  inputs.src-fltk-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-fltk-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-fltk-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
